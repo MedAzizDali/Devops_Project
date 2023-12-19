3a6831d2abedf89fb9cffb6fc1f3cf3aa80459f9
MedAzizDali <mohamedaziz.dali@polytechnicien.tn>
2023-12-19 19:02:03 +0100

    latest

--- auth/auth.js
+++ auth/auth.js
@@ -1,3 +1,4 @@
+// auth.js
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,13 +8,20 @@ const port = 4000;
 app.use(bodyParser.json());
 
 // Simulated authentication logic
-app.post('/auth', (req, res) => {
+const authenticate = (req, res) => {
   const { email } = req.body;
   // Simulated authentication check
   const authenticated = email && email.endsWith('@example.com');
   res.json({ authenticated });
-});
+};
+
+app.post('/auth', authenticate);
 
 app.listen(port, () => {
   console.log(`Authentication Service running on port ${port}`);
 });
+
+module.exports = { authenticate };
+
+
+
